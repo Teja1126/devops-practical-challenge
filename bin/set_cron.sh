92abab4c9cd2a853d7aef07501c7289595f17c10
Teja1126 <75604501+Teja1126@users.noreply.github.com>
2021-09-16 02:39:42 +0530

    Update set_cron.sh

--- bin/set_cron.sh
+++ bin/set_cron.sh
@@ -1,3 +1,8 @@
+if [[ $# != 1 ]]; then
+  echo "usage ./set_cron.sh <time_in_min>"
+  echo "ex :- ./set_cron.sh 10"
+fi
+
 cp ./nginx_status_check.py /opt/
 { crontab -l; echo "*/$1 * * * * python3 nginx_status_check.py"; } | crontab -
 
